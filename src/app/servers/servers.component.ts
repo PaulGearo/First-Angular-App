6400d58e2e487d1e8163dd3ab0ed30b35773de21
Paul Gearo <PaulGearo@gmail.com>
2020-06-29 16:56:05 -0400

    testing with two-way bindings

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverName = 'TestString';
+  serverCreationStatus = 'no server was created';
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = 'server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: InputEvent){
+    console.log(event);//this.serverCreationStatus =
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
